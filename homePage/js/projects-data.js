0b5b1536eacd7805372e933d4f66073a89830b5c
Israel Eliav <israele24h@gmail.com>
2025-02-22 23:35:20 +0200

    Add Bootstrap icon to Countries project preview page

--- homePage/js/projects-data.js
+++ homePage/js/projects-data.js
@@ -1,101 +1,101 @@
 const projects = {
     "1": {
-       title: "Coffee Shop",
-       description: "Unlock a world of indulgence with our café store! Sign up now and treat yourself to a complimentary cup of coffee on us. Get ready to sip, savor, and enjoy the ultimate coffee experience!",
-       image: "./images/coffeeShop.png",
-       projectPath: "../project1/index.html",
-       downloadPath: "../project1/project1.rar",
-       technologies: ["html5", "css3-alt"]
-   },
-   "2": {
-       title: "Fashion Store",
-       description: "Your one-stop destination for fashion. Discover latest trends and unique styles, all in one place!",
-       image: "./images/fashionStore.png",
-       projectPath: "../project2/index.html",
-       downloadPath: "../project2/project2.rar",
-       technologies: ["html5", "css3-alt"]
-   },
-   "3": {
-       title: "Gift Store",
-       description: "Find the perfect gift for every occasion. Make moments special with our curated collection!",
-       image: "./images/giftStore.png",
-       projectPath: "../project3/index.html",
-       downloadPath: "../project3/project3.rar",
-       technologies: ["html5", "css3-alt"]
-   },
-   "4": {
-       title: "Custom Website",
-       description: "A showcase of modern web design. Clean, responsive, and user-friendly interface.",
-       image: "./images/customWebsite.png",
-       projectPath: "../project4/index.html",
-       downloadPath: "../project4/project4.rar",
-       technologies: ["html5", "css3-alt"]
-   },
-   "5": {
-       title: "Consulting Services",
-       description: "Professional consulting services for your business needs. Expert guidance, proven results.",
-       image: "./images/consultingServices.png",
-       projectPath: "../project5/index.html",
-       downloadPath: "../project5/project5.rar",
-       technologies: ["html5", "css3-alt"]
-   },
-   "6": {
-       title: "Digital Marketing",
-       description: "Boost your online presence with our digital marketing solutions. Reach, engage, and grow!",
-       image: "./images/digitalMarketing.png",
-       projectPath: "../project6/index.html",
-       downloadPath: "../project6/project6.rar",
-       technologies: ["html5", "css3-alt"]
-   },
-   "7": {
-       title: "Nature Gallery",
-       description: "Explore the beauty of nature through our stunning photography collection.",
-       image: "./images/natureGallery.png",
-       projectPath: "../project7/index.html",
-       downloadPath: "../project7/project7.rar",
-       technologies: ["html5", "css3-alt"]
-   },
-   "8": {
-       title: "Facebook Website",
-       description: "Connect with your passion, share your journey, and inspire others!",
-       image: "./images/facebook.png",
-       projectPath: "../project8/index.html",
-       downloadPath: "../project8/project8.rar",
-       technologies: ["html5", "css3-alt", "sass"]
-   },
-   "project_weatherly": {
-       title: "weatherly app",
-       description: "Get real-time weather updates for any location. Plan your day with confidence!",
-       image: "./images/weatherly.png",
-       projectPath: "../project_weatherly/index.html",
-       downloadPath: "../project_weatherly/project_weatherly.rar",
-       technologies: ["html5", "css3-alt", "sass", "js"]
-   },
-   "countries_project": {
+    title: "Coffee Shop",
+    description: "Unlock a world of indulgence with our café store! Sign up now and treat yourself to a complimentary cup ofcoffee on us. Get ready to sip, savor, and enjoy the ultimate coffee experience!",
+    image: "./images/coffeeShop.png",
+    projectPath: "../project1/index.html",
+    downloadPath: "../project1/project1.rar",
+    technologies: ["html5", "css3-alt"]
+    },
+    "2": {
+    title: "Fashion Store",
+    description: "Your one-stop destination for fashion. Discover latest trends and unique styles, all in one place!",
+    image: "./images/fashionStore.png",
+    projectPath: "../project2/index.html",
+    downloadPath: "../project2/project2.rar",
+    technologies: ["html5", "css3-alt"]
+    },
+    "3": {
+    title: "Gift Store",
+    description: "Find the perfect gift for every occasion. Make moments special with our curated collection!",
+    image: "./images/giftStore.png",
+    projectPath: "../project3/index.html",
+    downloadPath: "../project3/project3.rar",
+    technologies: ["html5", "css3-alt"]
+    },
+    "4": {
+    title: "Custom Website",
+    description: "A showcase of modern web design. Clean, responsive, and user-friendly interface.",
+    image: "./images/customWebsite.png",
+    projectPath: "../project4/index.html",
+    downloadPath: "../project4/project4.rar",
+    technologies: ["html5", "css3-alt"]
+    },
+    "5": {
+    title: "Consulting Services",
+    description: "Professional consulting services for your business needs. Expert guidance, proven results.",
+    image: "./images/consultingServices.png",
+    projectPath: "../project5/index.html",
+    downloadPath: "../project5/project5.rar",
+    technologies: ["html5", "css3-alt"]
+    },
+    "6": {
+    title: "Digital Marketing",
+    description: "Boost your online presence with our digital marketing solutions. Reach, engage, and grow!",
+    image: "./images/digitalMarketing.png",
+    projectPath: "../project6/index.html",
+    downloadPath: "../project6/project6.rar",
+    technologies: ["html5", "css3-alt"]
+    },
+    "7": {
+    title: "Nature Gallery",
+    description: "Explore the beauty of nature through our stunning photography collection.",
+    image: "./images/natureGallery.png",
+    projectPath: "../project7/index.html",
+    downloadPath: "../project7/project7.rar",
+    technologies: ["html5", "css3-alt"]
+    },
+    "8": {
+    title: "Facebook Website",
+    description: "Connect with your passion, share your journey, and inspire others!",
+    image: "./images/facebook.png",
+    projectPath: "../project8/index.html",
+    downloadPath: "../project8/project8.rar",
+    technologies: ["html5", "css3-alt", "sass"]
+    },
+    "project_weatherly": {
+    title: "weatherly app",
+    description: "Get real-time weather updates for any location. Plan your day with confidence!",
+    image: "./images/weatherly.png",
+    projectPath: "../project_weatherly/index.html",
+    downloadPath: "../project_weatherly/project_weatherly.rar",
+    technologies: ["html5", "css3-alt", "sass", "js"]
+    },
+    "countries_project": {
     title: "Countries Explorer",
     description: "Explore countries and their flags around the world. Save your favorites with just one click!",
     image: "./images/countries.png",
     projectPath: "../countries_project/index.html",
     downloadPath: "../countries_project/countries_project.rar",
-    technologies: ["html5", "css3-alt", "js"]
-},
-"pageBuilder": {
+    technologies: ["html5", "css3-alt", "bootstrap", "js"]
+    },
+    "pageBuilder": {
     title: "Page Builder",
     description: "Create and customize pages easily with this page builder. Save your work and share it with others.",
     image: "./images/pageBuilder.PNG",
     projectPath: "../pageBuilder/index.html",
     downloadPath: "../pageBuilder/pageBuilder.rar",
     technologies: ["html5", "css3-alt", "js"]
-},
-"mathNexus": {
+    },
+    "mathNexus": {
     title: "Math Nexus Game",
     description: "Boost your math skills through fun, fast-paced exercises. Start from basic calculations, advance through levels, and watch your progress grow!",
     image: "./images/MathNexus.png",
     projectPath: "../mathNexus/index.html",
     downloadPath: "../mathNexus/mathNexus.rar",
     technologies: ["html5", "css3-alt", "sass", "js"]
-},
-"XO_Arena": {
+    },
+    "XO_Arena": {
         title: "XO Arena",
         description: "In a world where X's and O's wage an eternal battle, legends tell of an ancient game that holds the power to crown the ultimate strategist. Step into XO Arena, where three mighty AI guardians test the wit and wisdom of those who dare to challenge them. Every move shapes your destiny, every victory brings you closer to immortal glory. Will you answer the call and claim your place in the eternal battle of minds?",
         image: "./images/ticTacToe.png",
@@ -103,7 +103,7 @@ const projects = {
         downloadPath: "../XO_Arena/XO_Arena.rar",
         technologies: ["html5", "css3-alt", "bootstrap", "js"]
     },
-"memory_challenge": {
+    "memory_challenge": {
         title: "Memory Challenge",
         description: "Test your memory and focus with this classic card-matching game. Flip cards, match pairs and set new records!",
         image: "./images/memory_challenge.png",
